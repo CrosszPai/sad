c24e3a9be28f045f6b8faf3c2da3fa442a89cafc
Narawit Triprom <61010556@kmitl.ac.th>
2021-10-21 18:54:37 +0700

    structual work

---
+++ assignment/SoftArchDesign_str/src/edu/parinya/softarchdesign/structural/TaxPayingHealthcareWorker.java
@@ -0,0 +1,7 @@
+package edu.parinya.softarchdesign.structural;
+
+public class TaxPayingHealthcareWorker extends HealthcareWorkerDecorator {
+    public TaxPayingHealthcareWorker(HealthcareWorkerDecorator timeNicholas) {
+        super();
+    }
+}
---
+++ assignment/SoftArchDesign_str/src/edu/parinya/softarchdesign/structural/TimeLoggingHealthcareWorker.java
@@ -0,0 +1,7 @@
+package edu.parinya.softarchdesign.structural;
+
+public class TimeLoggingHealthcareWorker extends HealthcareWorkerDecorator {
+    public TimeLoggingHealthcareWorker(Surgeon nicholas) {
+        super();
+    }
+}
